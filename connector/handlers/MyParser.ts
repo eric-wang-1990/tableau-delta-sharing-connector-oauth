9b3588039dcc8f4e9e2548c86b0fc28a0f0a2dfd
terence-db <terence.wong@databricks.com>
2023-02-23 13:43:27 -0800

    combine datatables

--- connector/handlers/MyParser.ts
+++ connector/handlers/MyParser.ts
@@ -2,7 +2,7 @@ import { DataContainer, log, ParseOptions, AsyncParser, DataTable } from '@table
 
 export default class MyParser extends AsyncParser {
   async parse(fetcherResult: DataTable, { dataContainer }: ParseOptions): Promise<DataContainer> {
-    log(`parsing started for '${fetcherResult.name}'`)
+    log(`Parser called for ${fetcherResult.name}`)
     const containerBuilder = AsyncParser.createContainerBuilder(dataContainer)
     const { isNew, tableBuilder } = containerBuilder.getTable(fetcherResult.name)
     if (isNew) {
