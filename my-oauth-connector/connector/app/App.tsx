dacbe09cc9a9b7dcb74d4ee85484969dab971cae
eric wang <e.wang@databricks.com>
2024-09-16 12:44:44 -0700

    update

--- my-oauth-connector/connector/app/App.tsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom/client'
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import ConnectorView from './components/ConnectorView'
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<ConnectorView />)
